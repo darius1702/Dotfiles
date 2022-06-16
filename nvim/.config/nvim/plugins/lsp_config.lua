311e750d40ea9132620b962946a63438472a7086
Darius Schefer <darius1702@gmail.com>
2022-06-16 16:37:07 +0200

    cleanup config

--- nvim/.config/nvim/plugins/lsp_config.lua
+++ nvim/.config/nvim/plugins/lsp_config.lua
@@ -1,3 +1,6 @@
+-- lsp-isntaller
+require("nvim-lsp-installer").setup {}
+
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap=true, silent=true }
