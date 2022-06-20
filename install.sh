a171546adfb5772af6448b845e19d6b3564f1483
Darius Schefer <darius1702@gmail.com>
2022-06-20 18:55:31 +0200

    update tmux config

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 # zsh-syntax-highlighting
- git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ./zsh/.config/zsh/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ./zsh/.config/zsh/zsh-syntax-highlighting
 
  # tpm
 git clone https://github.com/tmux-plugins/tpm ./tmux/.tmux/plugins/tpm
 
- stow */
+stow */
