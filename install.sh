c7f919509eb422b97250ea6e31c26c60a422b390
Darius Schefer <darius1702@gmail.com>
2023-01-20 16:41:48 +0100

    Update install script

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+set -e
 # zsh better vi mode
-git clone https://github.com/jeffreytse/zsh-vi-mode.git ./zsh/.config/zsh/zsh-vi-mode
+[ -d ./zsh/.config/zsh/zsh-vi-mode ] && echo "[INFO] zsh-vi-mode was cloned already, skipping..." || git clone https://github.com/jeffreytse/zsh-vi-mode.git ./zsh/.config/zsh/zsh-vi-mode
 
 # tpm
-git clone https://github.com/tmux-plugins/tpm ./tmux/.tmux/plugins/tpm
+[ -d ./tmux/.tmux/plugins/tpm ] && echo "[INFO] tpm was cloned already, skipping..." || git clone https://github.com/tmux-plugins/tpm ./tmux/.tmux/plugins/tpm
 
 # link everything
 stow */
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -2,4 +2,3 @@ source ~/.config/zsh/.zshrc
 
 [ -f "/home/darius/.ghcup/env" ] && source "/home/darius/.ghcup/env" # ghcup-env
 export PATH=~/go/bin:$PATH
-source $HOME/kitcar/kitcar-init/config/zshrc_private
