b18162119ba8a7d5a6492224c741d274dd956703
Darius Schefer <darius1702@gmail.com>
2023-02-12 12:38:51 +0100

    Clean up lazy config

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -9,7 +9,8 @@ if not vim.loop.fs_stat(lazypath) then
     lazypath,
   })
 end
-vim.opt.rtp:prepend(lazypath)require("lazy").setup({
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup({
 
   -- Colors
   {
@@ -26,20 +27,23 @@ vim.opt.rtp:prepend(lazypath)require("lazy").setup({
   "nvim-lualine/lualine.nvim",
 
   -- Ros
-  "tadachs/mutils.nvim",
   {
     'darius1702/ros-nvim',
-    config = function() require("ros-nvim").setup({only_workspace = true}) end,
+    opts = { only_workspace = true },
+    branch = "devel",
     dependencies = { "tadachs/mutils.nvim" },
   },
 
   -- LaTeX
-  "lervag/vimtex",
+  {
+    "lervag/vimtex",
+    ft = "tex",
+  },
 
   -- Autopairs
   {
     "windwp/nvim-autopairs",
-    config = function() require('nvim-autopairs').setup{} end,
+    config = true,
   },
 
   -- Neorg
@@ -49,19 +53,14 @@ vim.opt.rtp:prepend(lazypath)require("lazy").setup({
     build = ":Neorg sync-parsers",
     opts = {
       load = {
-        ["core.defaults"] = {}, -- Loads default behaviour
-        ["core.norg.dirman"] = { -- Manages Neorg workspaces
+        ["core.defaults"] = {},
+        ["core.norg.dirman"] = {
           config = {
             workspaces = {
               notes = "~/Documents/Notes/neorg/",
             },
           },
         },
-        -- ["core.norg.concealer"] = {
-        --   config = {
-        --     folds = false,
-        --   },
-        -- },
         ["core.norg.completion"] = {
           config = {
             engine = "nvim-cmp",
@@ -73,64 +72,59 @@ vim.opt.rtp:prepend(lazypath)require("lazy").setup({
     dependencies = { { "nvim-lua/plenary.nvim" } },
   },
 
--- Syntax
-"numToStr/Comment.nvim",
-{
-  "kylechui/nvim-surround",
-  config = function() require('nvim-surround').setup{} end,
-},
-{
-  "norcalli/nvim-colorizer.lua",
-  config = function() require('colorizer').setup{
-    '*'; -- highlight in all files
-  } end,
-},
+  -- Syntax
+  "numToStr/Comment.nvim",
+  {
+    "kylechui/nvim-surround",
+    config = true,
+  },
+  {
+    "norcalli/nvim-colorizer.lua",
+    opts = '*';
+  },
 
--- Git
-"airblade/vim-gitgutter",
-{
-  "TimUntersberger/neogit",
-  config = function() require('neogit').setup{} end,
-  keys = {
-    { "<leader>gs", "<cmd>Neogit<cr>", desc = "Neogit" }
+  -- Git
+  "airblade/vim-gitgutter",
+  {
+    "TimUntersberger/neogit",
+    config = true,
+    keys = {
+      { "<leader>gs", "<cmd>Neogit<cr>", desc = "Neogit" }
+    }
+  },
+
+  -- tmux
+  "preservim/vimux",
+  "christoomey/vim-tmux-navigator",
+
+  -- Utils
+  "nvim-lua/plenary.nvim",
+  "nvim-lua/popup.nvim",
+  "nvim-telescope/telescope.nvim",
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+  "nvim-treesitter/playground",
+
+  -- LSPZero
+  {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v1.x',
+    dependencies = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},             -- Required
+      {'williamboman/mason.nvim'},           -- Optional
+      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},         -- Required
+      {'hrsh7th/cmp-nvim-lsp'},     -- Required
+      {'hrsh7th/cmp-buffer'},       -- Optional
+      {'hrsh7th/cmp-path'},         -- Optional
+      {'saadparwaiz1/cmp_luasnip'}, -- Optional
+      {'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+      -- Snippets
+      {'L3MON4D3/LuaSnip'},             -- Required
+      {'rafamadriz/friendly-snippets'}, -- Optional
+    }
   }
-},
-
--- tmux
-"preservim/vimux",
-"christoomey/vim-tmux-navigator",
-
--- Utils
-"nvim-lua/plenary.nvim",
-"nvim-lua/popup.nvim",
-"nvim-telescope/telescope.nvim",
-{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-"nvim-treesitter/playground",
-
--- LSPZero
-{
-  'VonHeikemen/lsp-zero.nvim',
-  branch = 'v1.x',
-  dependencies = {
-    -- LSP Support
-    {'neovim/nvim-lspconfig'},             -- Required
-    {'williamboman/mason.nvim'},           -- Optional
-    {'williamboman/mason-lspconfig.nvim'}, -- Optional
-
-    -- Autocompletion
-    {'hrsh7th/nvim-cmp'},         -- Required
-    {'hrsh7th/cmp-nvim-lsp'},     -- Required
-    {'hrsh7th/cmp-buffer'},       -- Optional
-    {'hrsh7th/cmp-path'},         -- Optional
-    {'saadparwaiz1/cmp_luasnip'}, -- Optional
-    {'hrsh7th/cmp-nvim-lua'},     -- Optional
-
-    -- Snippets
-    {'L3MON4D3/LuaSnip'},             -- Required
-    {'rafamadriz/friendly-snippets'}, -- Optional
-  }
-}
 })
-
-
-
