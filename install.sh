50aa7e1e94bb47b23befae026549199d5a52032c
Darius Schefer <darius1702@gmail.com>
2023-02-12 11:30:15 +0100

    remove tpm and zsh-autosuggestions because unused

--- install.sh
+++ install.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
-
 set -e
+
 # zsh better vi mode
 [ -d ./zsh/.config/zsh/zsh-vi-mode ] && echo "[INFO] zsh-vi-mode was cloned already, skipping..." || git clone https://github.com/jeffreytse/zsh-vi-mode.git ./zsh/.config/zsh/zsh-vi-mode
-
-# tpm
-[ -d ./tmux/.tmux/plugins/tpm ] && echo "[INFO] tpm was cloned already, skipping..." || git clone https://github.com/tmux-plugins/tpm ./tmux/.tmux/plugins/tpm
 
 # link everything
 stow */
