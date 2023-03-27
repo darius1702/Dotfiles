d7a3ced3274e33386a7c78a5648c583929fd0c0e
Darius Schefer <darius1702@gmail.com>
2023-03-27 04:11:36 +0200

    Add nvim mappings to open/close quickfix list and jump to next/previous error

--- nvim/.config/nvim/lua/config/remap.lua
+++ nvim/.config/nvim/lua/config/remap.lua
@@ -1,6 +1,12 @@
 vim.cmd([[vnoremap J :m '>+1<CR>gv=gv]])
 vim.cmd([[vnoremap K :m '<-2<CR>gv=gv]])
 
+-- Quickfix list
+vim.keymap.set("n", "<leader>co", ":copen<CR>", { silent = true })
+vim.keymap.set("n", "<leader>cc", ":cclose<CR>", { silent = true })
+vim.keymap.set("n", "]c", ":cnext<CR>", { silent = true })
+vim.keymap.set("n", "[c", ":cprev<CR>", { silent = true })
+
 vim.keymap.set("n", "<leader>p", "\"+p",{ silent = true })
 vim.keymap.set("x", "<leader>p", "\"_dP",{ silent = true })
 vim.keymap.set("x", "<leader>y", "\"+y",{ silent = true })
