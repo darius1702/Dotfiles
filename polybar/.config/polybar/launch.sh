3742dc3120af4f3ecf37f131e8d6c8317623ed7d
Darius Schefer <darius1702@gmail.com>
2023-05-10 23:57:51 +0200

    Make Polybar use multiple monitors correctly

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -2,9 +2,16 @@
 
 # Terminate already running bar instances
 # If all your bars have ipc enabled, you can use
-polybar-msg cmd quit
+# polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
-# killall -q polybar
+killall -q polybar
+
+docked=false
+for monitor in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+  [ "$monitor" != "eDP" ] && docked=true
+done
 
 # Launch bar(s)
-polybar 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
+[ $docked = true ] && polybar left 2>&1 | tee -a /tmp/polybar1.log & disown
+[ $docked = true ] && polybar right 2>&1 | tee -a /tmp/polybar1.log & disown
