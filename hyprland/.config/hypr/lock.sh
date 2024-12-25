3fbb1d4b5a1fb19a55fb6a761a34ae7b42663a85
Darius Schefer <darius1702@gmail.com>
2024-12-25 14:32:29 +0100

    Hyprland updates
    - start.sh replaced with exec-once calls
    - hypridle and hyprlock update, no more lock.sh
    - fprintd integration

--- hyprland/.config/hypr/lock.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# swayidle timeout 5 'hyprctl dispatch dpms off' \
-#          resume 'hyprctl dispatch dpms on' &
-
-swayidle timeout 5 'hyprctl dispatch exec systemctl suspend' &
-pid=$!
-hyprlock
-kill $pid
