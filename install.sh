6146d890699f1e2e8ce5712682c0d7c3c830344d
Darius Schefer <darius1702@gmail.com>
2025-07-09 23:36:35 +0200

    Updates

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -xe
 
-# rofi theme
+# Rofi themes will stow to here
 mkdir -p ~/.local/share/rofi/themes/
 
-# link everything
+### Misc ###
 stow gitconfig
 stow nvim
 stow zsh
@@ -16,7 +16,12 @@ stow foot
 stow waybar
 stow mako
 stow iwgtk
-stow rofi
+stow rofi       # rofi-wayland package on Arch
+stow satty
+
+stow systemd
+systemctl --user daemon-reload
+systemctl --user enable --now ssh-agent.service
 
 echo "[!] ./iwd/main.conf belongs in /etc/iwd/main.conf"
 echo "[!] ./systemd-boot/arch.conf belongs in /boot/loader/entries/ (adjust root UUID)"
