601da2ee9b0f0635b02f9ba6dadfa3cf89d32121
Darius Schefer <darius1702@gmail.com>
2026-05-14 16:24:41 +0200

    Updates
    Env vars

--- hyprland/.config/hypr/environment.lua
+++ hyprland/.config/hypr/environment.lua
@@ -1,12 +1,19 @@
 -- cursors
-hl.env("HYPRCURSOR_THEME","Bibata-Modern-Ice")
-hl.env("HYPRCURSOR_SIZE","20")
-hl.env("XCURSOR_THEME","Bibata-Modern-Ice")
-hl.env("XCURSOR_SIZE","20")
+hl.env("HYPRCURSOR_THEME", "Bibata-Modern-Ice")
+hl.env("HYPRCURSOR_SIZE", "20")
+hl.env("XCURSOR_THEME", "Bibata-Modern-Ice")
+hl.env("XCURSOR_SIZE", "20")
 
--- Some default env vars
-hl.env("XDG_SESSION_TYPE","wayland")
-hl.env("XDG_CURRENT_DESKTOP","Hyprland") -- Otherwise there is some "XDG_CURRENT_DESKTOP is managed externally ..." error
-hl.env("MOZ_ENABLE_WAYLAND","1")
-hl.env("GDK_BACKEND","wayland")
-hl.env("SSH_AUTH_SOCK","$XDG_RUNTIME_DIR/ssh-agent.socket")
+-- XDG stuff
+hl.env("XDG_SESSION_TYPE", "wayland")
+hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
+hl.env("XDG_SESSION_DESKTOP", "Hyprland")
+
+-- tell apps to use wayland
+hl.env("QT_QPA_PLATFORM", "wayland;xcb")
+hl.env("MOZ_ENABLE_WAYLAND", "1")
+hl.env("GDK_BACKEND", "wayland,x11,*")
+hl.env("SDL_VIDEODRIVER", "wayland")
+
+-- ssh agent
+hl.env("SSH_AUTH_SOCK", "$XDG_RUNTIME_DIR/ssh-agent.socket")
